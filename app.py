da2a64a80f1711f44f6036b6dec5d513887abbd5
mahmudurmahid <m.mahid@icloud.com>
2025-05-05 12:29:14 +0100

    gather user data from form

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    print(request.method)
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        employment_status = request.form["employment_status"]
+        print(first_name, last_name, email, date, employment_status)
+
     return render_template("index.html")
 
 
